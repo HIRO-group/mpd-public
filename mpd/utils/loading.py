100cb5824640d60edac36859313419bd0936ea2c
Gilberto Briscoe-Martinez <gibr6485@colorado.edu>
2024-04-30 11:21:30 -0600

    added request yaml loading

--- mpd/utils/loading.py
+++ mpd/utils/loading.py
@@ -1,6 +1,21 @@
 import yaml
+import pdb
 
 
 def load_params_from_yaml(path: str):
     with open(path, "r") as stream:
         return yaml.load(stream, Loader=yaml.FullLoader)
+
+def get_start_goal_from_yaml(path: str):
+    with open(path, "r") as stream:
+            try:
+                data = yaml.load(
+                    stream, Loader=yaml.Loader
+                )  # DO NOT USE UNTRUSTED YAMLS
+            except yaml.YAMLError as exc:
+                print(exc)
+    start_pos = data['start_state']['joint_state']['position'][0:7]
+    goal_pos = []
+    for joint in data['goal_constraints'][0]['joint_constraints']:
+        goal_pos.append(joint['position'])
+    return start_pos, goal_pos
